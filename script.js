9c54124c5c300e199d8bb34c06c32fca3c5b11d8
merlia19 <merlindeborah19@gmail.com>
2022-05-03 00:44:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//light/dark mode//
+function changeMode(){
+    let mybody = document.body;
+    mybody.classList.toggle('mydark')
+}
+//coupon//
+function loadCoupon(){
+    document.getElementById("coupon").style.display="block"
+}
+
+function closeCoupon(){
+    document.getElementById("coupon").style.display="none"
+}
